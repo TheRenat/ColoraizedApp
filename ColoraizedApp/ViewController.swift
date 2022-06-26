81f9173096783f654949f247c101ccb85035e2cc
TheRenat <therenat@mail.ru>
2022-06-26 23:46:58 +0300

    Finished logic part

--- ColoraizedApp/ViewController.swift
+++ ColoraizedApp/ViewController.swift
@@ -30,70 +30,40 @@ class ViewController: UIViewController {
         greenColorSliderAction()
         blueColorSliderAction()
         
-       // redColorSliderAction()
+        colorView.layer.cornerRadius = 10
         
-        
-        colorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
-        
-        
-        
-        //colorView.backgroundColor?.rgba = (0.1, 0.2, 0.3, 1.0)
-        
-//        var cet: CGFloat = 1.0
-//        var red = redColorSlider.value
-//        var red = redColorSlider.value
-//        var red = redColorSlider.value
-//        
-//        colorView.backgroundColor!.getRed(redColorSlider?.value, green: redColorSlider?.value, blue: redColorSlider?.value, alpha: cet?)
-        
-        //let cs = CGColorSpace(name: colorView)
-//        let cc = CGColor(colorSpace: colorView.backgroundColor! CGColorSpace, components: [0.9, 0.1, 0.9, 1.0])
-//        let color = UIColor(cgColor: cc!)
-        
-//        colorView.backgroundColor = UIColor(CGColor((colorSpace: colorView.backgroundColor! ,components: [0.9, 0.1, 0.9, 1.0])))
-//
-//
-//        let cs: CGColorSpace = (colorView.backgroundColor! as! CGColorSpace?)!
-//        let cc = CGColor(colorSpace: cs!, CGColorSpace, components: [0.9, 0.1, 0.9, 1.0])
-//        let color = UIColor(cgColor: cc!)
-//
-        
-        
-        //colorView.backgroundColor = UIColor.tintColor(redColorSlider.value)
+        redColorSlider.minimumTrackTintColor = .red
+        greenColorSlider.minimumTrackTintColor = .green
+        blueColorSlider.minimumTrackTintColor = .blue
     }
 
     @IBAction func redColorSliderAction() {
         redColorValue.text = "\(redColorSlider.value)"
-        colorView.backgroundColor = .red
-        colorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
+        redColorValue.text = (String(format: "%.2f", redColorSlider.value))
+        
+        sliderActionSetup()
     }
     
     @IBAction func greenColorSliderAction() {
         greenColorValue.text = "\(greenColorSlider.value)"
-        colorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
+        greenColorValue.text = (String(format: "%.2f", greenColorSlider.value))
+        
+        sliderActionSetup()
     }
     
     @IBAction func blueColorSliderAction() {
         blueColorValue.text = "\(blueColorSlider.value)"
-        colorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
+        blueColorValue.text = (String(format: "%.2f", blueColorSlider.value))
+        
+        sliderActionSetup()
     }
     
-    
     private func sliderSetup(color: UISlider) {
-        color.value = 0.3
+        color.value = Float.random(in: color.minimumValue...color.maximumValue)
         color.maximumValue = 1.0
         color.minimumValue = 0.0
     }
-
-//}
-//extension UIColor {
-//    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
-//        var red: CGFloat = 0
-//        var green: CGFloat = 0
-//        var blue: CGFloat = 0
-//        var alpha: CGFloat = 0
-//        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-//
-//        return (red, green, blue, alpha)
-//    }
+    private func sliderActionSetup() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
+    }
 }

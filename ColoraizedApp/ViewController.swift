9cdd7447a6d6ebd2d46631c64efcb9c26c8afab7
TheRenat <therenat@mail.ru>
2022-06-27 19:56:36 +0300

    remove extra code

--- ColoraizedApp/ViewController.swift
+++ ColoraizedApp/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var colorView: UIView!
     
     @IBOutlet var redColorValue: UILabel!
@@ -36,23 +36,20 @@ class ViewController: UIViewController {
         greenColorSlider.minimumTrackTintColor = .green
         blueColorSlider.minimumTrackTintColor = .blue
     }
-
+    
     @IBAction func redColorSliderAction() {
-        redColorValue.text = "\(redColorSlider.value)"
         redColorValue.text = (String(format: "%.2f", redColorSlider.value))
         
         sliderActionSetup()
     }
     
     @IBAction func greenColorSliderAction() {
-        greenColorValue.text = "\(greenColorSlider.value)"
         greenColorValue.text = (String(format: "%.2f", greenColorSlider.value))
         
         sliderActionSetup()
     }
     
     @IBAction func blueColorSliderAction() {
-        blueColorValue.text = "\(blueColorSlider.value)"
         blueColorValue.text = (String(format: "%.2f", blueColorSlider.value))
         
         sliderActionSetup()
@@ -60,8 +57,6 @@ class ViewController: UIViewController {
     
     private func sliderSetup(color: UISlider) {
         color.value = Float.random(in: color.minimumValue...color.maximumValue)
-        color.maximumValue = 1.0
-        color.minimumValue = 0.0
     }
     private func sliderActionSetup() {
         colorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)

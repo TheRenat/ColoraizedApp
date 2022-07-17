76ae4414bfc15cf3e2501202284f100ae1de4d11
TheRenat <therenat@mail.ru>
2022-07-17 21:55:36 +0300

    add logic

--- ColoraizedApp/ViewControllers/SettingViewController.swift
+++ ColoraizedApp/ViewControllers/SettingViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SettingViewController: UIViewController {
     
+    //MARK: - IBOutlets
     @IBOutlet var colorView: UIView!
     
     @IBOutlet var redColorValue: UILabel!
@@ -19,15 +20,26 @@ class SettingViewController: UIViewController {
     @IBOutlet var greenColorSlider: UISlider!
     @IBOutlet var blueColorSlider: UISlider!
     
-    var colorisedView: UIColor!
+    @IBOutlet var redColorTF: UITextField!
+    @IBOutlet var greenColorTF: UITextField!
+    @IBOutlet var blueColorTF: UITextField!
+    
+    
     var delegate: SettingViewControllerDelegate!
+    var colorisedView: UIColor!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colorView.layer.cornerRadius = 10
+        
+        setValue(for: redColorSlider, greenColorSlider, blueColorSlider)
+    }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
     @IBAction func doneButtonPressed() {
@@ -46,6 +58,17 @@ class SettingViewController: UIViewController {
         redColorValue.text = (String(format: "%.2f", redColorSlider.value))
         greenColorValue.text = (String(format: "%.2f", greenColorSlider.value))
         blueColorValue.text = (String(format: "%.2f", blueColorSlider.value))
+    }
+    
+    private func setValue(for colorSlider: UISlider...) {
+        let ciColor = CIColor(color: colorisedView)
+        colorSlider.forEach { slider in
+            switch slider {
+            case redColorSlider: redColorSlider.value = Float(ciColor.red)
+            case greenColorSlider: greenColorSlider.value = Float(ciColor.green)
+            default: blueColorSlider.value = Float(ciColor.blue)
+            }
+        }
     }
 }
 

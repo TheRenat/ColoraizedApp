9682c3183ae4bb52e6555ec768258d640e909cb6
TheRenat <therenat@mail.ru>
2022-07-17 22:37:00 +0300

    add logic

--- ColoraizedApp/ViewControllers/SettingViewController.swift
+++ ColoraizedApp/ViewControllers/SettingViewController.swift
@@ -24,7 +24,7 @@ class SettingViewController: UIViewController {
     @IBOutlet var greenColorTF: UITextField!
     @IBOutlet var blueColorTF: UITextField!
     
-    
+    //MARK: Public Properties
     var delegate: SettingViewControllerDelegate!
     var colorisedView: UIColor!
     
@@ -35,6 +35,8 @@ class SettingViewController: UIViewController {
         colorView.layer.cornerRadius = 10
         
         setValue(for: redColorSlider, greenColorSlider, blueColorSlider)
+        setValue(for: redColorValue, greenColorValue, blueColorValue)
+        setValue(for: redColorTF, greenColorTF, blueColorTF)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -42,24 +44,37 @@ class SettingViewController: UIViewController {
         view.endEditing(true)
     }
     
+    //MARK: IBActions
     @IBAction func doneButtonPressed() {
         delegate.setColor(colorView.backgroundColor ?? .white)
         dismiss(animated: true)
     }
     
     @IBAction func rgbSlider(_ sender: UISlider) {
+        switch sender {
+        case redColorSlider:
+            setValue(for: redColorValue)
+            setValue(for: redColorTF)
+        case greenColorSlider:
+            setValue(for: greenColorValue)
+            setValue(for: greenColorTF)
+        default:
+            setValue(for: blueColorValue)
+            setValue(for: blueColorTF)
+        }
+        setColor()
+    }
+    
+    //MARK: Private Methods
+    private func setColor() {
         colorView.backgroundColor = UIColor(
             red: CGFloat(redColorSlider.value),
             green: CGFloat(greenColorSlider.value),
             blue: CGFloat(blueColorSlider.value),
             alpha: 1
         )
-        
-        redColorValue.text = (String(format: "%.2f", redColorSlider.value))
-        greenColorValue.text = (String(format: "%.2f", greenColorSlider.value))
-        blueColorValue.text = (String(format: "%.2f", blueColorSlider.value))
     }
-    
+
     private func setValue(for colorSlider: UISlider...) {
         let ciColor = CIColor(color: colorisedView)
         colorSlider.forEach { slider in
@@ -70,5 +85,38 @@ class SettingViewController: UIViewController {
             }
         }
     }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redColorValue: label.text = getString(from: redColorSlider)
+            case greenColorValue: label.text = getString(from: greenColorSlider)
+            default: label.text = getString(from: blueColorSlider)
+            }
+        }
+    }
+    
+    private func setValue(for textFields: UITextField...) {
+        textFields.forEach { textField in
+            switch textField {
+            case redColorTF: textField.text = getString(from: redColorSlider)
+            case greenColorTF: textField.text = getString(from: greenColorSlider)
+            default: textField.text = getString(from: blueColorSlider)
+            }
+        }
+    }
+
+    private func getString(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
+    private func didTapDone() {
+        view.endEditing(true)
+    }
+    
+    
 }
+
+
+
 
